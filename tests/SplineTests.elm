22b514ccdc60e0c10f33fb08eb21ed9ae82cb1da
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2018-12-11 17:32:25 +0100

    Added test for spline

--- tests/SplineTests.elm
+++ tests/SplineTests.elm
@@ -10,10 +10,19 @@ s =
     Spline.make 10 16 [ 12, 23, 34, 45, 56, 67, 78 ]
 
 
+testSpline : Float -> Float -> (() -> Expect.Expectation)
+testSpline x y =
+    \_ -> Expect.equal y <| Spline.evaluate s x
+
+
 suite : Test
 suite =
     describe "Splines"
         [ test "Stub" <|
             \_ ->
                 Expect.equal 12 <| Spline.evaluate s 10
+        , describe "Should get original values back"
+            [ test "11 -> 23" <|
+                testSpline 11 23
+            ]
         ]
