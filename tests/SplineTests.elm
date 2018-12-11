6bb6b280855ef898fed73e0006afc2949a21cd53
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2018-12-11 16:18:52 +0100

    Stubbed Spline module

---
+++ tests/SplineTests.elm
@@ -0,0 +1,13 @@
+module SplineTests exposing (suite)
+
+import Expect exposing (..)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Splines"
+        [ test "Stub" <|
+            \_ ->
+                Expect.equal (1 + 1) 2
+        ]
