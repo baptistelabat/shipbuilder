9b425a955f1153cab28f52ed04c181f47fa66ca9
PICARD Michel <michel.picard@sirehna.com>
2018-12-12 11:17:57 +0100

    add lights to the scene

--- shipBuilder/js/main.js
+++ shipBuilder/js/main.js
@@ -1129,6 +1129,19 @@ let initRenderer = function () {
 
 let initScene = function () {
     scene = new THREE.Scene();
+
+    var lights = [];
+    			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+    			lights[ 0 ].position.set( 100, 200, 100 );
+    			lights[ 1 ].position.set( 100, -200, 100 );
+    			lights[ 2 ].position.set( 0, 0, -200 );
+
+    			scene.add( lights[ 0 ] );
+    			scene.add( lights[ 1 ] );
+    			scene.add( lights[ 2 ] );
 }
 
 let initCameras = function () {
