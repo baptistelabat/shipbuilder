b21a064c7548a51756bb58b815df86e7e8e3fd58
PICARD Michel <michel.picard@sirehna.com>
2019-01-24 19:20:57 +0100

    add HullSliceUtilities

---
+++ src/HullSliceUtilities.elm
@@ -0,0 +1,131 @@
+module HullSliceUtilities exposing
+    ( areaTrapezoid
+    , trapezoidCentroid
+    , yTrapezoid
+    , zTrapezoid
+    )
+
+
+square : Float -> Float
+square n =
+    n ^ 2
+
+
+areaTrapezoid : ( Float, Float ) -> ( Float, Float ) -> Float
+areaTrapezoid ( z1, y1 ) ( z2, y2 ) =
+    -- http://mathworld.wolfram.com/Trapezoid.html
+    0.5 * (y1 + y2) * (z2 - z1)
+
+
+zTrapezoid : ( Float, Float ) -> ( Float, Float ) -> Float
+zTrapezoid ( z1, y1 ) ( z2, y2 ) =
+    -- http://mathworld.wolfram.com/Trapezoid.html
+    let
+        a =
+            abs y1
+
+        b =
+            abs y2
+
+        c =
+            abs (z2 - z1)
+
+        d =
+            sqrt (square c + square (y2 - y1))
+
+        dz =
+            z2 - z1
+    in
+    (b + 2 * a) / (3 * (a + b)) * dz
+
+
+yTrapezoid : ( Float, Float ) -> ( Float, Float ) -> Float
+yTrapezoid ( z1, y1 ) ( z2, y2 ) =
+    -- http://mathworld.wolfram.com/Trapezoid.html
+    let
+        a =
+            abs y1
+
+        b =
+            abs y2
+
+        c =
+            abs (z2 - z1)
+
+        d =
+            sqrt (square c + square (y2 - y1))
+
+        dz =
+            z2 - z1
+    in
+    b / 2.0 + (2 * a + b) * (square c - square d) / 6 * (square b - square a)
+
+
+trapezoidCentroid : ( Float, Float ) -> ( Float, Float ) -> ( Float, Float, Float )
+trapezoidCentroid p1 p2 =
+    -- calculate centroid z, y, and area
+    ( zTrapezoid p1 p2, yTrapezoid p1 p2, areaTrapezoid p1 p2 )
+
+
+fromNZ : Float -> { c | zmin : Float, zmax : Float, y : List Float } -> Float
+fromNZ z curve =
+    -- convert z normalized to absolute value
+    -- z in [0,1]
+    z * (curve.zmax - curve.zmin) + curve.zmin
+
+
+toNZ : Float -> { c | zmin : Float, zmax : Float, y : List Float } -> Float
+toNZ z curve =
+    -- convert absolute z to normalized
+    (z - curve.zmin) / (curve.zmax - curve.zmin)
+
+
+trapezoidCentroidForList : List ( Float, Float ) -> List ( Float, Float, Float )
+trapezoidCentroidForList list =
+    -- calculate (z,y,a) on a list. return a list of 3-uplet
+    case list of
+        p1 :: p2 :: xs ->
+            trapezoidCentroid p1 p2 :: trapezoidCentroidForList xs
+
+        _ ->
+            []
+
+
+totalArea : List ( Float, Float, Float ) -> Float
+totalArea l =
+    l
+        |> List.map (\( z, y, a ) -> a)
+        |> List.sum
+
+
+totalProductZ : List ( Float, Float, Float ) -> Float
+totalProductZ l =
+    l
+        |> List.map (\( z, y, a ) -> z * a)
+        |> List.sum
+
+
+totalProductY : List ( Float, Float, Float ) -> Float
+totalProductY l =
+    l
+        |> List.map (\( z, y, a ) -> y * a)
+        |> List.sum
+
+
+zyaForSlice : List ( Float, Float ) -> ( Float, Float, Float )
+zyaForSlice list =
+    -- return zcentroid, ycentroid, and total area for the list
+    let
+        l1 =
+            trapezoidCentroidForList list
+
+        ta =
+            totalArea l1
+
+        tz =
+            totalProductZ l1
+
+        ty =
+            totalProductY l1
+    in
+    ( tz / ta, ty / ta, ta )
