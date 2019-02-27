473d3364929c6c2757d81668e8f452f5beec0a0b
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-02-27 13:08:39 +0100

    Switch export order

--- src/HullSliceUtilities.elm
+++ src/HullSliceUtilities.elm
@@ -1,8 +1,8 @@
 module HullSliceUtilities exposing
     ( areaTrapezoid
-    ,  hullVolume
     , demormalizedHullSlice
     , denormalizedHSList
+    , hullVolume
     , intersectBelow
     , kBx
     ,  kBz
